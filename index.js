fb43e126c50e42331f68b287cae14bb33e0c55f0
Hannah <hannahmgrout@gmail.com>
2022-05-18 20:05:16 -0700

    commit tree dca84df3abf6d3e03462b18f4bee8dfc75046ddd

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+      let newString = string.charAt(0).toUpperCase() + string.slice(1)
+      return newString
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-' ]+/g, "")
+  }
+
+  static titleize (string){
+    let splitString = string.split(' ')
+    let firstWord = splitString[0].charAt(0).toUpperCase() + splitString[0].slice(1)
+    let words = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'and', 'from']
+  
+
+    for(let i= 1; i<splitString.length; i++){
+      if (words.includes(splitString[i])){
+          splitString[i]
+        }
+        else{
+          splitString[i] = splitString[i][0].toUpperCase() + splitString[i].substr(1)
+        }
+    }  
+
+    let newString = splitString.splice(1)
+    let updatedString = newString.join(' ')
+    return firstWord + ' ' + updatedString
+  }
 }
